865924073b02d995cc12c7e7112176b7e2e66b6b
KBomba <KBomba@users.noreply.github.com>
2014-06-20 21:12:03 +0200

    Seems like I missed these two ;)

---
+++ CudaProfitCalc/JsonControl.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace CudaProfitCalc
+{
+    static class JsonControl
+    {
+        public static T DownloadSerializedApi<T>(string address) where T : new()
+        {
+            string jsonData;
+            using (WebClient web = new WebClient())
+            {
+               jsonData = web.DownloadString(address);
+            }
+
+            T newT = !string.IsNullOrEmpty(jsonData) ? JsonConvert.DeserializeObject<T>(jsonData) : new T();
+            return newT;
+        }
+
+        public static T GetSerializedApiFile<T>(string location) where T : new()
+        {
+            T newT = new T();
+
+            using (StreamReader file = File.OpenText(location))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                newT = (T)serializer.Deserialize(file, typeof(T));
+            }
+            
+            return newT;
+        }
+    }
+}
---
+++ CudaProfitCalc/MiscJson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CudaProfitCalc
+{
+    class MiscJson
+    {
+    }
+
+    class HashRateJson
+    {
+        [JsonProperty("HashRateList")]
+        public Dictionary<HashAlgo.Algo, double> List { get; set; }
+    }
+
+    class ApiJson
+    {
+        [JsonProperty("ApiSettings")]
+        public Dictionary<string, string> ApiSettings { get; set; }
+
+        [JsonProperty("CheckedApis")]
+        public Dictionary<string, bool> CheckedApis { get; set; } 
+    }
+}
