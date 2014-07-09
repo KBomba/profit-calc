e8ceb50e1f0c5aa226b1b336651f7107123ebb5c
KBomba <KBomba@users.noreply.github.com>
2014-07-09 07:09:24 +0200

    Missing file, thx Cayars. Isn't used ATM, so forgot about it.

---
+++ ProfitCalc/ApiControl/CryptToday.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ProfitCalc.ApiControl
+{
+    internal class CryptToday
+    {
+        [JsonProperty("cols")]
+        public List<Col> Cols { get; set; }
+
+        [JsonProperty("rows")]
+        public List<Row> Rows { get; set; }
+
+        public class Col
+        {
+
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+            [JsonProperty("label")]
+            public string Label { get; set; }
+
+            [JsonProperty("pattern")]
+            public string Pattern { get; set; }
+
+            [JsonProperty("type")]
+            public string Type { get; set; }
+        }
+
+        public class Row
+        {
+
+            [JsonProperty("c")]
+            public List<Result> Results { get; set; }
+            public class Result
+            {
+
+                [JsonProperty("v")]
+                public string Btc { get; set; }
+            }
+        }
+    }
+}
